20679e5233122e1f94b7d60339a62c91cae21fe3
Marcelo Augusto <marcelo.augusto.453@outlook.com>
2025-09-14 16:55:26 -0300

    bug: correct error in abstract class

---
+++ src/lib/classes/AbstractDataSource.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractDataSource(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def start(self):
+        raise NotImplementedError("Method not implemented")
+    
+    @abstractmethod
+    def get_data(self):
+        raise NotImplementedError("Method not implemented")
+    
+    @abstractmethod
+    def transform_data_to_df(self):
+        raise NotImplementedError("Method not implemented")
+    
+    @abstractmethod
+    def save_data(self):
+        raise NotImplementedError("Method not implemented")
